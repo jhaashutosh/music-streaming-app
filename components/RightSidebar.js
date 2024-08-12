e800a961b71f62aefacb77d82408c1796e138273
jhaashutosh <jhaashutosh58@gmail.com>
2024-08-12 16:05:44 +0530

    changed the profile pic, fixed the width issue for responsiveness

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -1,12 +1,12 @@
 import styles from '../styles/module/rightSidebar.module.css';
 import ListContainer from './ListContainer';
 
-const RightSidebar = ({currentMusic, setCurrentMusic}) => {
+const RightSidebar = () => {
 
   return (
     <div className={`${styles.sidebar} flex flex-col p-6`}>
       <div className="w-full profile flex gap-4 items-center justify-start">
-        <img src="/images/profile.jpeg" alt="Profile" className="rounded-full w-8 h-8" />
+        <img src="/images/dummy-profile.jpg" alt="Profile" className="rounded-full w-8 h-8" />
         <h4 className="text-white text-center">Ashutosh</h4>
       </div>
       <div className="w-full overflow-y-auto">
